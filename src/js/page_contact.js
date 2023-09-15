cf2cdd507f797aae2d3d33758e24d4bfe8665064
Shivam Gangwani <shivam.gangwani2804@gmail.com>
2023-09-15 12:28:25 +0530

    UI Updates

--- src/js/page_contact.js
+++ src/js/page_contact.js
@@ -9,7 +9,7 @@ export default function() {
     let head_h1 = createElementEx("h1", "", "", "Contact Us");
     head.appendChild(head_h1);
 
-    let body = createElementEx("div", "page-copy-body", [], "Reach out to us at +69420-EAT-FOOD");
+    let body = createElementEx("div", "page-copy-body", [], "Reach out to us at +69420-EAT-FOOD, or email us at notavalidemailaddress@yolo.kong");
     setPageHeadImage(headImg);
     return [head, body];
 }
--- src/js/page_home.js
+++ src/js/page_home.js
@@ -8,7 +8,7 @@ export default function() {
     let head_h1 = createElementEx("h1", "", [], "Homepage");
     head.appendChild(head_h1);
 
-    let body = createElementEx("div", "page-copy-body", [], "Welcome to the Homepage!");
+    let body = createElementEx("div", "page-copy-body", [], "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sequi veritatis accusamus ut assumenda ipsum incidunt commodi, culpa aspernatur, unde, praesentium rerum quasi a eos fugiat nulla magnam tempore. Obcaecati?");
     setPageHeadImage(headImg);
     return [head, body];
 }
